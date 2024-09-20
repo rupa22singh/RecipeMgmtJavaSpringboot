ec694320267bd8a4f24366132b11e5704d9f917a
rupa22singh <rupa.22.singh@gmail.com>
2024-09-20 10:25:30 +0530

    Spring Security dependency added.

--- src/main/java/com/rsing/recipe/config/SecurityConfig.java
+++ src/main/java/com/rsing/recipe/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package com.rsing.recipe.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
 
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-//        return httpSecurity.authorizeHttpRequests(request->
-//                request.requestMatchers("/h2-console/**").permitAll()
-//                        .anyRequest().authenticated())
-//                .headers(headers -> headers.frameOptions(Customizer.withDefaults()))
-//                .csrf(AbstractHttpConfigurer::disable).build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(request->
+                request.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(Customizer.withDefaults()))
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
